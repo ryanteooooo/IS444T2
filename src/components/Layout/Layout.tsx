5191c4a6ee4ff538136fafc8e7d1f2305c248da3
axeltpb <teopinbo@hotmail.com>
2024-10-28 17:27:13 +0800

    profile page structure is done

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+// src/components/Layout/Layout.tsx
 
-// components
+import React from 'react';
 import Header from '../Header/Header';
 
 // interfaces
 interface IProps {
   children: React.ReactNode;
+  className?: string; // Optional className prop
 }
 
-const Layout = ({ children }: IProps): React.JSX.Element => (
+const Layout = ({ children, className }: IProps): React.JSX.Element => (
   <>
-    <div className='bg' />
-    <div className='content flex flex-col'>
+    <div className={`bg ${className}`} /> {/* Apply className to the background div */}
+    <div className={`content flex flex-col ${className}`}>
       <div className='container'>
         <Header />
         {children}
