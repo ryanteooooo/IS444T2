224e6c141bc465d8ed9f1ef510ca865ea91d637e
Fiona Li <fionalibq2407@gmail.com>
2024-10-30 15:06:46 +0800

    Change SignIn.tsx to Signin.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import SignIn from './pages/SignIn';
+import SignIn from './pages/Signin';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home'; 
 import Add from './pages/Add';
