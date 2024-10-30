e91ab933772fa1d77ede91ea6b166bea40b42157
ryanteooooo <ryan.teo.2022@scis.smu.edu.sg>
2024-10-30 15:18:49 +0800

    c

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -16,18 +16,18 @@ const Actions = (): React.JSX.Element => (
       </Link>
       <span className='text-shadow'>Exchange</span>
     </div>
-    {/* <div className='circle no-select flex flex-col flex-v-center flex-h-center'>
-      <Link to='/home' className='flex flex-v-center flex-h-center'>
+    <div className='circle no-select flex flex-col flex-v-center flex-h-center'>
+      <Link to='/recommendation' className='flex flex-v-center flex-h-center'>
         <span className='material-symbols-outlined'>page_info</span>
       </Link>
-      <span className='text-shadow'>Details</span>
-    </div> */}
-    <div className='circle no-select flex flex-col flex-v-center flex-h-center'>
+      <span className='text-shadow'>Reco</span>
+    </div>
+    {/* <div className='circle no-select flex flex-col flex-v-center flex-h-center'>
       <button type='button' className='flex flex-v-center flex-h-center'>
         <span className='material-symbols-outlined'>more_horiz</span>
       </button>
       <span className='text-shadow'>For you</span>
-    </div>
+    </div> */}
   </div>
 );
 
